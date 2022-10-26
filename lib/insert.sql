4b566ee96a5f4078c1249f08c5f7f14cc7148bd7
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-10-26 18:24:23 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr.Chocolate", 21, "M", "black" , "sanguine", TRUE),
+("Rowdy", 11, "M", "brown" , "choleric", TRUE),
+("Tabitha", 7, "F", "grey" , "phlegmatic", FALSE),
+("Sergeant Brown", 19, "M", "black" , "melancholic", TRUE),
+("Melissa", 17, "F", "white" , "sanguine", TRUE),
+("Grinch", 4, "M", "black" , "choleric", TRUE),
+("Wendy", 13, "F", "white" , "sanguine", FALSE),
+(NULL, NULL, "M", "brown" , "phlegmatic", TRUE);
